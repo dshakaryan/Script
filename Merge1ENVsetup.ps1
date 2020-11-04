80928d4767f0df5ce6e3d3da9b1d9430e3c6cb28
dshakaryan <45834622+dshakaryan@users.noreply.github.com>
2020-11-04 14:05:51 +0400

    Delete Merge1ENVsetup.ps1

--- Merge1ENVsetup.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "$(Get-Date) INFO: Installing IIS..."
-    Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
-    Remove-Website -Name 'Default Web Site'  
-    Remove-WebAppPool *
-    Write-Host "$(Get-Date) INFO: Downloading Visual C++ Redistributable 2017..."
-    Invoke-WebRequest -Uri 'https://stglobanetautomation.blob.core.windows.net/common/VC_redist.x64_2017.exe' `
-     -OutFile 'VC_redist.x64_2017.exe' -UseBasicParsing
-    Write-Host "$(Get-Date) INFO: Installing Visual C++ Redistributable 2017..."
-    ./VC_redist.x64_2017.exe /install /passive /norestart
